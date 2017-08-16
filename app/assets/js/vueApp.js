3c54bea936e568ade2c0f48481a349dfc6f490bb
Jarod Hargreaves <jarod.hargreaves@gmail.com>
2017-08-16 11:43:44 +0100

    Trim down to just one usergroup

--- app/assets/js/vueApp.js
+++ app/assets/js/vueApp.js
@@ -3,97 +3,70 @@ var htmlCreator = new Vue({
     data: {
         mainStructure: "",
         outputExpanded: false,
-        mod1: [
-            {
-                headerExpanded: true,
-                currency: { gbp: true, eur: false, usd: false },
-                collector: { collector: true, noncollector: true },
-                content: [
-                    {
-                        desktopImg:
-                            "https://www.emmabridgewater.co.uk/content/ebiz/eb/resources/images/homepages/170810_cherries_desktop.jpg",
-                        mobileImg:
-                            "https://www.emmabridgewater.co.uk/content/ebiz/eb/resources/images/homepages/170810_cherries_mobile.jpg",
-                        link:
-                            "https://www.emmabridgewater.co.uk/icat/patcherries",
-                        altText: "50% off Summer Cherries"
-                    },
-                    { desktopImg: "", mobileImg: "", link: "", altText: "" },
-                    { desktopImg: "", mobileImg: "", link: "", altText: "" }
-                ]
-            },
-            {
-                headerExpanded: false,
-                currency: { gbp: false, eur: true, usd: true },
-                collector: { collector: false, noncollector: true },
-                content: [
-                    { desktopImg: "", mobileImg: "", link: "", altText: "" },
-                    { desktopImg: "", mobileImg: "", link: "", altText: "" },
-                    { desktopImg: "", mobileImg: "", link: "", altText: "" }
-                ]
-            },
-            {
-                headerExpanded: false,
-                currency: { gbp: false, eur: true, usd: true },
-                collector: { collector: true, noncollector: false },
-                content: [
-                    { desktopImg: "", mobileImg: "", link: "", altText: "" },
-                    { desktopImg: "", mobileImg: "", link: "", altText: "" },
-                    { desktopImg: "", mobileImg: "", link: "", altText: "" }
-                ]
-            }
-        ]
+        mod1: {
+            headerExpanded: true,
+            currency: { gbp: true, eur: false, usd: false },
+            collector: { collector: true, noncollector: true },
+            content: [
+                {
+                    desktopImg:
+                        "https://www.emmabridgewater.co.uk/content/ebiz/eb/resources/images/homepages/170810_cherries_desktop.jpg",
+                    mobileImg:
+                        "https://www.emmabridgewater.co.uk/content/ebiz/eb/resources/images/homepages/170810_cherries_mobile.jpg",
+                    link: "https://www.emmabridgewater.co.uk/icat/patcherries",
+                    altText: "50% off Summer Cherries"
+                },
+                { desktopImg: "", mobileImg: "", link: "", altText: "" },
+                { desktopImg: "", mobileImg: "", link: "", altText: "" }
+            ]
+        }
     },
     computed: {
         output: function() {
             var text = ""
+            var collectors = [
+                this.mod1.collector.collector ? "hp-collector " : "",
+                this.mod1.collector.noncollector ? "hp-noncollector " : ""
+            ].join("")
 
-            this.mod1.forEach(function(usergroup) {
-                var collectors = [
-                    usergroup.collector.collector ? "hp-collector " : "",
-                    usergroup.collector.noncollector ? "hp-noncollector " : ""
-                ].join("")
+            var currencies = [
+                this.mod1.currency.gbp ? "hp-gbp " : "",
+                this.mod1.currency.eur ? "hp-eur " : "",
+                this.mod1.currency.usd ? "hp-usd " : ""
+            ].join("")
 
-                var currencies = [
-                    usergroup.currency.gbp ? "hp-gbp " : "",
-                    usergroup.currency.eur ? "hp-eur " : "",
-                    usergroup.currency.usd ? "hp-usd " : ""
-                ].join("")
+            text +=
+                "<div class='" +
+                collectors.trim() +
+                "'>" +
+                "\n  <div class='" +
+                currencies +
+                "hp-slick'>"
 
+            this.mod1.content.forEach(function(details) {
                 text +=
-                    "<div class='" +
-                    collectors.trim() +
+                    "\n    <div>" +
+                    "\n      <a href='" +
+                    details.link +
                     "'>" +
-                    "\n  <div class='" +
-                    currencies +
-                    "hp-slick'>"
-
-                usergroup.content.forEach(function(details) {
-                    text +=
-                        "\n    <div>" +
-                        "\n      <a href='" +
-                        details.link +
-                        "'>" +
-                        "\n        <picture>" +
-                        "\n          <source media='(max-width: 600px)' srcset='" +
-                        details.mobileImg +
-                        "'>" +
-                        "\n          <source media='(min-width: 600px)' srcset='" +
-                        details.desktopImg +
-                        "'>" +
-                        "\n          <img src='" +
-                        details.desktopImg +
-                        "' alt='" +
-                        details.altText +
-                        "'>" +
-                        "\n        </picture>" +
-                        "\n      </a>" +
-                        "\n    </div>"
-                })
-
-                text += "\n  </div>"
-                ;("\n</div>\n")
+                    "\n        <picture>" +
+                    "\n          <source media='(max-width: 600px)' srcset='" +
+                    details.mobileImg +
+                    "'>" +
+                    "\n          <source media='(min-width: 600px)' srcset='" +
+                    details.desktopImg +
+                    "'>" +
+                    "\n          <img src='" +
+                    details.desktopImg +
+                    "' alt='" +
+                    details.altText +
+                    "'>" +
+                    "\n        </picture>" +
+                    "\n      </a>" +
+                    "\n    </div>"
             })
+
+            text += "\n  </div>" + "\n</div>\n"
 
             return text
         }
@@ -104,6 +77,9 @@ var htmlCreator = new Vue({
         },
         selectFilter: function(filter, subfilter, item) {
             item[filter][subfilter] = !item[filter][subfilter]
+        },
+        selectOutput(element) {
+            element.select()
         },
         returnHTML: function() {
             this.outputExpanded = !this.outputExpanded
