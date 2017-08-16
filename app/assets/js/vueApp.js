d342437aa84a849fcd20bc79c871017a426aa3f9
Jarod Hargreaves <jarod.hargreaves@gmail.com>
2017-08-16 10:07:40 +0100

    Complete HTML generation

--- app/assets/js/vueApp.js
+++ app/assets/js/vueApp.js
@@ -47,26 +47,54 @@ var htmlCreator = new Vue({
     computed: {
         output: function() {
             var text = ""
-            text +=
-                "\n    <div>" +
-                "\n      <a href='" +
-                this.mod1[0].content[0].link +
-                "'>" +
-                "\n        <picture>" +
-                "\n          <source media='(max-width: 600px)' srcset='" +
-                this.mod1[0].content[0].mobileImg +
-                "'>" +
-                "\n          <source media='(min-width: 600px)' srcset='" +
-                this.mod1[0].content[0].desktopImg +
-                "'>" +
-                "\n          <img src='" +
-                this.mod1[0].content[0].desktopImg +
-                "' alt='" +
-                this.mod1[0].content[0].altText +
-                "'>" +
-                "\n        </picture>" +
-                "\n      </a>" +
-                "\n    </div>"
+
+            this.mod1.forEach(function(usergroup) {
+                var collectors = [
+                    usergroup.collector.collector ? "hp-collector " : "",
+                    usergroup.collector.noncollector ? "hp-noncollector " : ""
+                ].join("")
+
+                var currencies = [
+                    usergroup.currency.gbp ? "hp-gbp " : "",
+                    usergroup.currency.eur ? "hp-eur " : "",
+                    usergroup.currency.usd ? "hp-usd " : ""
+                ].join("")
+
+                text +=
+                    "<div class='" +
+                    collectors.trim() +
+                    "'>" +
+                    "\n  <div class='" +
+                    currencies +
+                    "hp-slick'>"
+
+                usergroup.content.forEach(function(details) {
+                    text +=
+                        "\n    <div>" +
+                        "\n      <a href='" +
+                        details.link +
+                        "'>" +
+                        "\n        <picture>" +
+                        "\n          <source media='(max-width: 600px)' srcset='" +
+                        details.mobileImg +
+                        "'>" +
+                        "\n          <source media='(min-width: 600px)' srcset='" +
+                        details.desktopImg +
+                        "'>" +
+                        "\n          <img src='" +
+                        details.desktopImg +
+                        "' alt='" +
+                        details.altText +
+                        "'>" +
+                        "\n        </picture>" +
+                        "\n      </a>" +
+                        "\n    </div>"
+                })
+
+                text += "\n  </div>"
+                ;("\n</div>\n")
+            })
+
             return text
         }
     },
