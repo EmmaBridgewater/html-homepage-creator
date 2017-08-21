33bba1578ae17c15ad59ba30664114a30426e118
Jarod Hargreaves <jarod.hargreaves@gmail.com>
2017-08-21 16:04:26 +0100

    Delete gulpfile.js

--- gulpfile.js
+++
@@ -1,9 +0,0 @@
-var gulp = require("gulp")
-	// sass = require('gulp-sass'),
-	// postcss = require("gulp-postcss"),
-	// autoprefixer = require("autoprefixer"),
-	// browserSync = require("browser-sync").create()
-
-gulp.task("default", function () {
-	console.log("hello")
-})
